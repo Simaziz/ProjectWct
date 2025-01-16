33ef27e73384a9f56e33d344519b5bb4638ce7a8
Aziz <aziznono77@gmail.com>
2025-01-16 16:08:37 +0700

    new

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
-
+/**@types{import('next').nextConfig}*/
+const nextConfig={
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+}
 module.exports = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
