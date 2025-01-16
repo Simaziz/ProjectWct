2f9eeb5645290b64a5cef11140dad7a60b73cb2d
Aziz <aziznono77@gmail.com>
2025-01-16 16:11:44 +0700

    new

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
+// Load environment variables from .env file
 require('dotenv').config();
-/**@types{import('next').nextConfig}*/
-const nextConfig={
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
-module.exports = {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL, // Expose the DATABASE_URL from the environment
   },
   experimental: {
-    turbo: false,
+    turbo: false, // Disable turbo mode if needed (can be removed if not necessary)
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint warnings during builds
   },
 };
+
+module.exports = nextConfig;
